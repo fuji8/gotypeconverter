ec6d9d915f9144e31ae57bc38ddb1d4114caa997
fuji <haya10.88ha@gmail.com>
2021-02-08 04:36:16 +0900

    add basic test

---
+++ testdata/src/basic/basic.go
@@ -0,0 +1,19 @@
+package main
+
+type BasicSRC struct {
+	foo int
+	bar float64
+	x   string
+	y   string
+}
+
+type BasicDST struct {
+	foo int
+	bar float32
+	x   string
+	y   bool
+}
+
+func main() {
+
+}
