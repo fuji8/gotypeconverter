248536fff35b510a5f2ff0e0229d7e28d76f6571
fuji8 <haya10.88ha@gmail.com>
2022-06-05 12:11:46 +0900

    wip

--- gotypeconverter_test.go
+++ gotypeconverter_test.go
@@ -2,11 +2,14 @@ package gotypeconverter
 
 import (
 	"flag"
+	"fmt"
+	"go/types"
 	"os"
 	"testing"
 
 	"github.com/fuji8/gotypeconverter/ui"
 	"github.com/gostaticanalysis/codegen/codegentest"
+	"golang.org/x/tools/go/packages"
 )
 
 var flagUpdate bool
@@ -24,4 +27,54 @@ func TestGenerator(t *testing.T) {
 	ui.TmpFilePath = codegentest.TestData() + "/src/a/tmp.go"
 	rs := codegentest.Run(t, codegentest.TestData(), Generator, "a")
 	codegentest.Golden(t, rs, flagUpdate)
+}
+
+func TypeOf4(pkg *types.Package, pkgname, typename string) types.Type {
+	if typename == "" {
+		return nil
+	}
+
+	if typename[0] == '*' {
+		obj := TypeOf4(pkg, pkgname, typename[1:])
+		if obj == nil {
+			return nil
+		}
+		return types.NewPointer(obj)
+	}
+
+	if typename[0] == '[' {
+		obj := TypeOf4(pkg, pkgname, typename[1:])
+		if obj == nil {
+			return nil
+		}
+		return types.NewSlice(obj)
+	}
+	if pkgname == "" {
+		obj := pkg.Scope().Lookup(typename)
+		return obj.Type()
+	}
+
+	obj := pkg.Scope().Lookup(typename)
+	if obj == nil {
+		return nil
+	}
+	return obj.Type()
+}
+
+func TestXXX(t *testing.T) {
+	tmp, err := packages.Load(&packages.Config{
+		Mode: packages.LoadAllSyntax,
+		Dir:  codegentest.TestData() + "/src/a",
+	}, "a")
+	tmp2 := TypeOf4(tmp[0].Types, "", "SRC")
+	fmt.Println(tmp2, err)
+	fmt.Println(tmp2.String())
+}
+
+func TestYYY(t *testing.T) {
+	tmp, err := packages.Load(&packages.Config{
+		Mode: packages.LoadAllSyntax,
+		Dir:  codegentest.TestData() + "/issue/x016",
+	}, "x016")
+	fmt.Println(tmp, err)
 }
--- testdata/issue/x016/x016.go
+++ testdata/issue/x016/x016.go
@@ -1,4 +1,4 @@
-package main
+package x016
 
 type SRC struct {
 	Foo int
