5f7efe358eab42f60952b18a11a9d461f0056714
fuji8 <haya10.88ha@gmail.com>
2022-09-11 16:27:07 +0900

    delete debug message

--- ui/ui.go
+++ ui/ui.go
@@ -50,8 +50,6 @@ func NoInfoGeneration(fm *ana.FuncMaker) (string, error) {
 
 	buf.Write(fm.WriteBytes())
 
-	fmt.Println(buf.String())
-
 	sortedData, err := sortFunction(buf.Bytes(), TmpFilePath)
 	if err != nil {
 		return "", err
